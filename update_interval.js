3f484efa31cdb71de301f58bd73f70d4b6759ccf
CPMDevelopment <127902421+CPMDevelopment@users.noreply.github.com>
2023-07-12 10:35:57 +1000

    Update update_interval.js

--- update_interval.js
+++ update_interval.js
@@ -18,9 +18,9 @@ $(document).ready(function() {
       url: 'http://192.168.20.11:5000/update_interval',
       data: { interval: interval, startTime: startTime, endTime: endTime, daysOfWeek: daysOfWeek },
       success: function(data) {
-        console.log(data); 
+        console.log(data);
       },
       dataType: 'json' 
     });
-  });
+  }); 
 });
