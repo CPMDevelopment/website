f200fa09a96d2ef44ca3764d65b91c5d0df6037b
CPMDevelopment <127902421+CPMDevelopment@users.noreply.github.com>
2023-07-19 14:58:42 +1000

    Update update_interval.js

--- update_interval.js
+++ update_interval.js
@@ -18,19 +18,19 @@ $(document).ready(function() {
       url: 'http://192.168.20.11:5000/update_interval',
       data: { interval: interval, startTime: startTime, endTime: endTime, daysOfWeek: daysOfWeek },
       success: function(data) {
-        console.log(data);  
+        console.log(data);    
       },
       dataType: 'json'   
     }); 
-  }); 
+  });
 
   // Fetch the latest image URL and update the image source
   const apiUrl = "https://example.com/api/latest-image";
-  fetch(apiUrl)
+  fetch(apiUrl) 
     .then(response => response.json())
-    .then(data => {
+    .then(data => { 
       document.getElementById("last-image").src = data.imageUrl;
-    })
+    }) 
     .catch(error => {
       console.error("Error fetching latest image:", error);
     });
