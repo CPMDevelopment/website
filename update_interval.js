780e5d929b4fe4d58cee3b4893d380891b0a0bea
CPMDevelopment <127902421+CPMDevelopment@users.noreply.github.com>
2023-08-23 14:43:26 +1000

    Update update_interval.js

--- update_interval.js
+++ update_interval.js
@@ -1,38 +1,31 @@
 $(document).ready(function() {
+  // ... Your existing script ...
+
   $('form').submit(function(event) {
     event.preventDefault();
-    const interval = $('#interval').val();
-    const startTime = $('#start-time').val();
-    const endTime = $('#end-time').val();
-    const daysOfWeek = {
-      monday: $('#monday').is(':checked'),
-      tuesday: $('#tuesday').is(':checked'),
-      wednesday: $('#wednesday').is(':checked'),
-      thursday: $('#thursday').is(':checked'), 
-      friday: $('#friday').is(':checked'),
-      saturday: $('#saturday').is(':checked'),
-      sunday: $('#sunday').is(':checked')
-    };
+    // ... Your existing form data extraction ...
+
     $.ajax({
       type: 'POST',
-      url: 'http://192.168.20.11:5000/update_interval',
+      url: 'http://192.168.20.11:5000/update_interval', // Flask app URL
       data: { interval: interval, startTime: startTime, endTime: endTime, daysOfWeek: daysOfWeek },
       success: function(data) {
         console.log(data);    
       },
       dataType: 'json' 
-      
-    });  
-  });  
+    });
+  });
 
-  // Fetch the latest image URL and update the image source
-  const apiUrl = "https://example.com/api/latest-image"; 
-  fetch(apiUrl)  
-    .then(response => response.json())  
-    .then(data => { 
-      document.getElementById("last-image").src = data.imageUrl;
-    })  
-    .catch(error => {
-      console.error("Error fetching latest image:", error);
-    }); 
+  $('#capture-button').click(function(event) {
+    event.preventDefault();
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://192.168.20.11:5000/capture', // Flask app URL
+      success: function(data) {
+        console.log(data);
+      },
+      dataType: 'json' 
+    });
+  });
 });
